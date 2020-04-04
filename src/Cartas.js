9e5a6ed1eb0b179aa3a6b07eeefda43966bca895
Al6 <acabdia6@users.noreply.github.com>
2020-04-05 00:19:34 +0200

    adding value

--- src/Cartas.js
+++ src/Cartas.js
@@ -5,7 +5,7 @@ import "./Cartas.css";
 
 function Cartas({value}) {
   return (
-  
+
     <main  className="carta grow center ma1">
         <p id="cardRender" className="noselect avenir dtc v-mid  tc w4 f1">{value}</p>
     </main>
