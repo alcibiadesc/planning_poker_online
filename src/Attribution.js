c59b09165fc2a8f9c657a6ce72f9b9ca00ed5b3a
Al6 <acabdia6@users.noreply.github.com>
2020-04-05 22:29:55 +0200

    link added

--- src/Attribution.js
+++ src/Attribution.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Attribution.css";
 
 
 
@@ -6,7 +7,7 @@ import React from "react";
 function Attribution() {
   return (
   <div className="mb4 tc">
-    <p className="f7 black-80 fw2 dib pa3 avenir" href="/">made with <span role="img" aria-label="heart"> ❤️</span>by Alcibiades Cabral Díaz</p>
+    <p className="f7 black-80 fw2 dib pa3 avenir" href="/">made with <span role="img" aria-label="heart"> ❤️</span>by <a className="link" href="https://www.linkedin.com/in/alcibiadesc">Alcibiades Cabral Díaz</a></p>
   </div>
 
   );
