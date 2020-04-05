2f8fa99138a0b26308adce7015ec8d2b859f6144
Al6 <acabdia6@users.noreply.github.com>
2020-04-05 22:22:57 +0200

    Updates

---
+++ precache-manifest.11babcb368e3ac53b95af239506a2686.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7652c21eb42dc14afc31ed0d365a5264",
+    "url": "/planning_poker_online/index.html"
+  },
+  {
+    "revision": "45313410515dce4da0e7",
+    "url": "/planning_poker_online/static/css/2.07548e1c.chunk.css"
+  },
+  {
+    "revision": "1fb409b8d0c809751188",
+    "url": "/planning_poker_online/static/css/main.f3f57c22.chunk.css"
+  },
+  {
+    "revision": "45313410515dce4da0e7",
+    "url": "/planning_poker_online/static/js/2.3679f829.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/planning_poker_online/static/js/2.3679f829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fb409b8d0c809751188",
+    "url": "/planning_poker_online/static/js/main.97db22e2.chunk.js"
+  },
+  {
+    "revision": "4e55b91609f0a857ab4c",
+    "url": "/planning_poker_online/static/js/runtime-main.d21be726.js"
+  }
+]);
