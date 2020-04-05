30cb95e0863b3c092c0ace597ed43c86a4a4c0dd
Al6 <acabdia6@users.noreply.github.com>
2020-04-05 17:52:03 +0200

    adding hooks in footer & eliminate the href

--- src/Footer.js
+++ src/Footer.js
@@ -1,21 +1,42 @@
-import React from "react";
-import "./Footer.css"
+import React, { useState } from "react";
+import "./Footer.css";
 
+function Footer() {
+  let [deck, setDeck] = useState("storyPoints");
 
-
-
-function BottomBar() {
   return (
-
     <footer id="footerBtm" className="bg-white black-80 tc avenir">
       <nav className="bt tc mw7 center ">
-        <a className="f5 f5-l link bg-animate black-80 hover-light-purple dib pa3 ph4-l linkBtm" href="/">StoryPoints</a>
-        <a className="f5 f5-l link bg-animate black-80 hover-light-purple dib pa3 ph4-l linkBtm" href="/portfolio">T-Shirt</a>
-        <a className="f5 f5-l link bg-animate black-80 hover-light-purple dib pa3 ph4-l linkBtm" href="/shop">Fibonacci</a>
+        <p
+          onClick={() => {
+            setDeck((deck = "storyPoints"));
+            console.log(`you changed the deck to: ${deck}`);
+          }}
+          className="noselect f5 f5-l link bg-animate black-80 hover-light-purple dib ph4 linkBtm"
+        >
+          StoryPoints
+        </p>
+        <p
+          onClick={() => {
+            setDeck((deck = "tshirt"));
+            console.log(`you changed the deck to: ${deck}`);
+          }}
+          className="noselect f5 f5-l link bg-animate black-80 hover-light-purple dib ph4 linkBtm"
+        >
+          T-Shirt
+        </p>
+        <p
+          onClick={() => {
+            setDeck((deck = "fibonacci"));
+            console.log(`you changed the deck to: ${deck}`);
+          }}
+          className="noselect f5 f5-l link bg-animate black-80 hover-light-purple dib ph4 linkBtm"
+        >
+          Fibonacci
+        </p>
       </nav>
     </footer>
   );
-
 }
 
-export default BottomBar;
+export default Footer;
