0db062904169b37ef234642c9647a5761c1da80b
Al6 <acabdia6@users.noreply.github.com>
2020-04-07 14:24:09 +0200

    log value card

--- src/components/Cartas.js
+++ src/components/Cartas.js
@@ -1,14 +1,26 @@
 import React from "react";
 import "./Cartas.css";
 
+function Cartas({ value }) {
 
+const useCardValue = (event) => {
+    console.log(value);
+}
 
-function Cartas({value}) {
   return (
-
-    <div  className="carta grow center ma1">
-        <p id="cardRender" className="noselect avenir dtc v-mid  tc w4 f1">{value}</p>
+    <>
+    <div className="carta grow center ma1">
+      <p
+        id="cardRender"
+        onClick={useCardValue}
+        value={value}
+        className="noselect avenir dtc v-mid  tc w4 f1"
+      >
+        {value}
+      </p>
     </div>
+    </>
+
   );
 }
 
