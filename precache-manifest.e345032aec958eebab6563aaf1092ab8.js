b6bd83c15379160fa109775f2a11ac847dc5bb4b
Al6 <acabdia6@users.noreply.github.com>
2020-04-11 01:45:57 +0200

    Updates

---
+++ precache-manifest.e345032aec958eebab6563aaf1092ab8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e3ace08d0189ddacb892c811fd2839",
+    "url": "/planning_poker_online/index.html"
+  },
+  {
+    "revision": "7cae71dbcf6ae1a44f5f",
+    "url": "/planning_poker_online/static/css/2.07548e1c.chunk.css"
+  },
+  {
+    "revision": "54a390df73be2ef45e98",
+    "url": "/planning_poker_online/static/css/main.54525ffb.chunk.css"
+  },
+  {
+    "revision": "7cae71dbcf6ae1a44f5f",
+    "url": "/planning_poker_online/static/js/2.add3c4f8.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/planning_poker_online/static/js/2.add3c4f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54a390df73be2ef45e98",
+    "url": "/planning_poker_online/static/js/main.3f1d22ec.chunk.js"
+  },
+  {
+    "revision": "4e55b91609f0a857ab4c",
+    "url": "/planning_poker_online/static/js/runtime-main.d21be726.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/planning_poker_online/precache-manifest.facf1cd4194c86633425fcd1b969b105.js"
+  "/planning_poker_online/precache-manifest.e345032aec958eebab6563aaf1092ab8.js"
 );
 
 self.addEventListener('message', (event) => {
