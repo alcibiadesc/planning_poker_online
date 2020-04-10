320343354eeffb6d88aa1e074ccfb175e2d8e45e
Al6 <acabdia6@users.noreply.github.com>
2020-04-10 21:19:05 +0200

    refactor

--- src/components/Attribution.js
+++ src/components/Attribution.js
@@ -1,15 +1,22 @@
 import React from "react";
 import "./Attribution.css";
 
-
-
-
 function Attribution() {
   return (
-  <div className="mb4 tc">
-    <p className="f7 black-80 fw2 dib pa3 avenir" href="/">made with <span role="img" aria-label="heart"> ❤️</span>by <a className="link" href="https://www.linkedin.com/in/alcibiadesc">Alcibiades Cabral Díaz</a> in Quarantine</p>
-  </div>
-
+    <div className="mb4 tc">
+      <p className="f7 black-80 fw2 dib pa3 avenir" href="/">
+        made with{" "}
+        <span role="img" aria-label="heart">
+          {" "}
+          ❤️
+        </span>
+        by{" "}
+        <a className="link" href="https://www.linkedin.com/in/alcibiadesc">
+          Alcibiades Cabral Díaz
+        </a>{" "}
+        in Quarantine
+      </p>
+    </div>
   );
 }
 
