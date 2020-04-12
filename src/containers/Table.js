2a08cad56e28c5597d0d48a4397b76b48b6f51f2
Al6 <acabdia6@users.noreply.github.com>
2020-04-13 01:02:13 +0200

    REFACTOR: Separate in a new component Custom Deck Menu

--- src/containers/Table.js
+++ src/containers/Table.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./Table.css";
 import CardList from "../components/CardList";
+import FooterMenu from "../components/FooterMenu.js";
+import CustomDeckMenu from "../components/CustomDeckMenu.js";
 import {
   storeCard,
   readStoredCards
 } from "../components/CustomLocalStorage.js";
-import FooterMenu from "../components/FooterMenu.js";
 
 // MAIN FUNCTION
 function Table() {
@@ -77,41 +78,16 @@ function Table() {
 
       {/* Custom Menu */}
 
-      <div className="w-100">
-        {addButtonNew ? (
-          <div className="w-100 tc center pa2">
-            <input
-              className="mb3 tc"
-              type="text"
-              value={newCardName}
-              onChange={useNewCardName}
-              onKeyPress={handleKeyPress}
-              maxLength="15"
-              placeholder="Card Value"
-            />
-            <br />
-            <button
-              href="#0"
-              type="Reset"
-              onClick={useAddNewCard}
-              className="f6 link dim ba ph3 pv2 mb2 dib dark-gray pa2 noselect "
-            >
-              New Card
-            </button>
-            <button
-              type="button"
-              name="button"
-              id="deleteAll"
-              className="f6 link dim ba ph3 pv2 mb2 dib dark-gray pa2 noselect"
-              onClick={deleteAllCustomCards}
-            >
-              Delete All
-            </button>
-          </div>
-        ) : (
-          ""
-        )}
-      </div>
+      {deck === "Custom" && (
+        <CustomDeckMenu
+          addButtonNew={addButtonNew}
+          newCardName={newCardName}
+          useNewCardName={useNewCardName}
+          handleKeyPress={handleKeyPress}
+          useAddNewCard={useAddNewCard}
+          deleteAllCustomCards={deleteAllCustomCards}
+        />
+      )}
 
       <CardList
         deckSelected={deck}
