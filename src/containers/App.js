3d3b4c94527e3a5820fd0b90984568d5a57006b6
Al6 <acabdia6@users.noreply.github.com>
2020-04-14 13:14:38 +0200

    Minor

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import TitleBar from "../components/TitleBar";
 import Attribution from "../components/Attribution";
-import Table from "../components/Table.js";
+import Table from "./Table";
 
 const App = () => {
   return (
