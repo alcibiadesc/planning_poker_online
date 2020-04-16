da90072bf72d72f6219fd1cac2461f22589e93d3
Al6 <acabdia6@users.noreply.github.com>
2020-04-16 10:53:27 +0200

    added id to baraja

--- src/components/Baraja.ts
+++ src/components/Baraja.ts
@@ -10,157 +10,195 @@ export const baraja = [
     // T-SHIRT
     value: "XS",
     deck: "T-Shirt",
+    id: "47102e28-161a-4e9c-a4ba-a674f6b470cc",
   },
   {
     value: "S",
     deck: "T-Shirt",
+    id: "a019c18f-9cf9-40a0-ac53-a06d89bfcb34",
   },
   {
     value: "M",
     deck: "T-Shirt",
+    id: "ff57165f-848a-4f44-9063-fa79afd4bc5a",
   },
   {
     value: "L",
     deck: "T-Shirt",
+    id: "0a277a04-8056-43c9-89f5-a367cc6332ee",
   },
   {
     value: "XL",
     deck: "T-Shirt",
+    id: "f9006926-d7cd-4b31-ba64-f2246b83969b",
   },
   {
     value: "XXL",
     deck: "T-Shirt",
+    id: "86ef4fdf-aca8-4239-8e6f-4859f792b4b8",
   },
 
   {
     value: "?",
     deck: "T-Shirt",
+    id: "05121a2f-141d-44d2-b24f-afc80339ccfc",
   },
   {
     value: "∞",
     deck: "T-Shirt",
+    id: "dd15f000-0413-4445-9741-04729e2729d6",
   },
   {
     value: "☕",
     deck: "T-Shirt",
+    id: "8516f051-4de8-4680-ae26-81c84a73087f",
   },
 
   {
     // STORYPOINTS
     value: "0",
     deck: "StoryPoints",
+    id: "ab6a0a04-6cb2-47df-be06-242460f2ea22",
   },
   {
     value: "1/2",
     deck: "StoryPoints",
+    id: "823c33b7-4f78-4234-b135-d30c9d0a5498",
   },
   {
     value: "1",
     deck: "StoryPoints",
+    id: "727fb625-ea09-445a-8f8b-2bfaca35d325",
   },
   {
     value: "2",
     deck: "StoryPoints",
+    id: "6e131d44-1c73-49fd-a341-e3f1670739fb",
   },
   {
     value: "3",
     deck: "StoryPoints",
+    id: "744f172b-1cf6-4069-b281-108cc2109a19",
   },
   {
     value: "5",
     deck: "StoryPoints",
+    id: "3312871a-dfc7-483a-8b83-b67905f5d8b4",
   },
   {
     value: "8",
     deck: "StoryPoints",
+    id: "22376c91-1ab8-4cea-8d82-3c01e9b33412",
   },
   {
     value: "13",
     deck: "StoryPoints",
+    id: "c640965a-50c1-4b87-8276-e5566ed7f51e",
   },
   {
     value: "20",
     deck: "StoryPoints",
+    id: "a2e6db3b-ca3d-44d6-9151-0478b48ec377",
   },
   {
     value: "40",
     deck: "StoryPoints",
+    id: "26dd18c8-612a-4749-a999-88d313352ab1",
   },
   {
     value: "100",
     deck: "StoryPoints",
+    id: "d47088b4-d4c7-4689-a541-45fb6bbfc91b",
   },
   {
     value: "?",
     deck: "StoryPoints",
+    id: "aedda75e-8692-44e2-95c8-e5f50dc9bf62",
   },
   {
     value: "∞",
     deck: "StoryPoints",
+    id: "7573bb47-bbc9-4af9-9fe9-26dc56cd0e0d",
   },
   {
     value: "☕",
     deck: "StoryPoints",
+    id: "69837d5a-b860-4ce1-bee3-0ebbb88c662b",
   },
   {
     // FIBONACCI
     value: "0",
     deck: "Fibonacci",
+    id: "596951b8-ba92-432c-9837-213d48be0be3",
   },
   {
     value: "1",
     deck: "Fibonacci",
+    id: "7e3df609-9f72-46fb-bd7b-96b6ef4e0c3d",
   },
   {
     value: "2",
     deck: "Fibonacci",
+    id: "1d3861cb-49ab-480e-9708-b8348586f949",
   },
   {
     value: "3",
     deck: "Fibonacci",
+    id: "ae5f0342-8083-4568-aa4e-ea1da3fb3e10",
   },
   {
     value: "5",
     deck: "Fibonacci",
+    id: "11de98a3-e2a8-477f-828e-5d6e5605fe38",
   },
   {
     value: "8",
     deck: "Fibonacci",
+    id: "1d6842f6-0cc2-464c-a24f-fb12fd6f6255",
   },
   {
     value: "13",
     deck: "Fibonacci",
+    id: "557da409-af3b-4e8c-8565-e0abf975b36c",
   },
   {
     value: "21",
     deck: "Fibonacci",
+    id: "02009c10-7075-49ca-a83e-896a81101178",
   },
   {
     value: "34",
     deck: "Fibonacci",
+    id: "4a512e63-46e2-4429-835b-345b19007f0c",
   },
   {
     value: "55",
     deck: "Fibonacci",
+    id: "59a1f034-aa1f-400b-bdb1-896e27ae1d92",
   },
   {
     value: "89",
     deck: "Fibonacci",
+    id: "91a0d645-ddb0-4908-a67e-5f8446c28c26",
   },
   {
     value: "144",
     deck: "Fibonacci",
+    id: "f908a387-fdb9-454a-8022-898b6ef73640",
   },
   {
     value: "?",
     deck: "Fibonacci",
+    id: "44195e63-2dea-40af-8c60-b8075b744247",
   },
   {
     value: "∞",
     deck: "Fibonacci",
+    id: "e18cf0a3-b36a-4a7f-9252-8b9eb64bc12d",
   },
   {
     value: "☕",
     deck: "Fibonacci",
+    id: "56e38d46-07ba-46da-97dd-f5ab9a7814be",
   },
 ];
